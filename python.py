fe7d112643bc0381b2be974d0c0ffe8f5ddd262b
Kay221a <shinkay177@gmail.com>
2023-02-03 22:58:59 +0700

    Say hello to master

---
+++ python.py
@@ -0,0 +1,18 @@
+Kondisi = True
+while Kondisi:
+    pilihan = input('''
+    ===================
+     Program Sederhana
+    ===================
+    Masukkan nama negara : ''')
+    if pilihan == 'Indonesia':
+        print('''
+Fakta Menarik tentang Indonesia
+
+Indonesia atau lebih lengkapnya Negara Kesatuan Republik Indonesia, adalah negara
+kepulauan di Asia Tenggara yang dilintasi gari khatulistiwa dan berada di antara 
+daratan benua Asia dan Oseania, sehingga dikenal sebagai negara lintas benua, serta
+antara Samudra Pasifik dan Samudra Hindia.''')
+     
+    elif pilihan == 'sudahlah':
+        Kondisi = False
