f0598747fec6eef39cda3cf3bb4f4246701c0c76
Kay221a <shinkay177@gmail.com>
2023-04-22 20:24:20 +0700

    Perubahan

--- python.py
+++ python.py
@@ -1,18 +1,7 @@
-Kondisi = True
-while Kondisi:
-    pilihan = input('''
-    ===================
-     Program Sederhana
-    ===================
-    Masukkan nama negara : ''')
-    if pilihan == 'Indonesia':
-        print('''
-Fakta Menarik tentang Indonesia
+var1 = int(input("Masukkan Angka : "))
+var2 = int(input("Masukkan Angka : "))
 
-Indonesia atau lebih lengkapnya Negara Kesatuan Republik Indonesia, adalah negara
-kepulauan di Asia Tenggara yang dilintasi gari khatulistiwa dan berada di antara 
-daratan benua Asia dan Oseania, sehingga dikenal sebagai negara lintas benua, serta
-antara Samudra Pasifik dan Samudra Hindia.''')
-     
-    elif pilihan == 'sudahlah':
-        Kondisi = False
+if var1 > var2:
+    print(var1, "Lebih besar dari", var2)
+else:
+    print(var2, "lebih besar dari", var1)
