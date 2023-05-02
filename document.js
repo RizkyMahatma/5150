bcfae49443aa361b0dd1a5315cf04074f444c8c7
Kay221a <shinkay177@gmail.com>
2023-05-03 03:53:43 +0700

    Dasar-dasar JS

--- document.js
+++ document.js
@@ -1,1 +1,18 @@
-document.write(5 + 6)
+//document.write(5 + 6)
+//window.alert("Waduh error")
+//console.log(5 + 6)
+var data_siswa;
+data_siswa = "Mochamad Amri Putra Karyadi";
+var angka = 12;
+var angka2= 13;
+let total = angka + angka2;
+document.writeln("12 + 13 = ",total)
+document.writeln("</br>")
+document.writeln("<textarea cols ='100' rows='10'>")
+document.writeln(data_siswa)
+document.writeln("TJKT3")
+document.writeln("ShinKay")
+document.writeln("</textarea")
+document.writeln('</br')
+var nama = "amri";
+document.writeln(nama)
