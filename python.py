907ad225d3e42c9a1523288e7b89f5a95e73f62d
Kay221a <shinkay177@gmail.com>
2023-05-06 13:42:00 +0700

    Perubahan hari sabtu

--- python.py
+++ python.py
@@ -1,7 +1,29 @@
-var1 = int(input("Masukkan Angka : "))
-var2 = int(input("Masukkan Angka : "))
+kondisi = True
+while kondisi :
+    pilihan = input('''
+    =============================
+     Data Si Pemilik Akun Github
+    =============================
+    1. Nama
+    2. Kelas
+    3. Sekolah
+    4. Asal
+    5. keluar
+    =============================
+    Masukkan pilihan kamu : '''
+    )
 
-if var1 > var2:
-    print(var1, "Lebih besar dari", var2)
-else:
-    print(var2, "lebih besar dari", var1)
+    if pilihan == "1":
+        print("    Amri")
+
+    elif pilihan == "2":
+        print("    X-TJKT3")
+
+    elif pilihan == "3":
+        print("    SMKN1 Cianjur")
+
+    elif pilihan == "4":
+        print("    Cianjur")
+
+    elif pilihan == "5":
+        kondisi = False
